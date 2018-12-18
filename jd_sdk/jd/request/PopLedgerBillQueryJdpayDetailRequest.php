7552d1f9ca684a00a4824c529ac780e910f8585e
shenfenglei <386738248@qq.com>
2018-12-18 16:07:54 +0800

    京东sdk包

---
+++ jd_sdk/jd/request/PopLedgerBillQueryJdpayDetailRequest.php
@@ -0,0 +1,85 @@
+<?php
+class PopLedgerBillQueryJdpayDetailRequest
+{
+	private $apiParas = array();
+	
+	public function getApiMethodName(){
+	  return "jingdong.pop.ledger.bill.queryJdpayDetail";
+	}
+	
+	public function getApiParas(){
+		return json_encode($this->apiParas);
+	}
+	
+	public function check(){
+		
+	}
+	
+	public function putOtherTextParam($key, $value){
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+                                                        		                                    	                   			private $type;
+    	                        
+	public function setType($type){
+		$this->type = $type;
+         $this->apiParas["type"] = $type;
+	}
+
+	public function getType(){
+	  return $this->type;
+	}
+
+                        	                        	                   			private $memberId;
+    	                        
+	public function setMemberId($memberId){
+		$this->memberId = $memberId;
+         $this->apiParas["memberId"] = $memberId;
+	}
+
+	public function getMemberId(){
+	  return $this->memberId;
+	}
+
+                        	                   			private $startDate;
+    	                        
+	public function setStartDate($startDate){
+		$this->startDate = $startDate;
+         $this->apiParas["startDate"] = $startDate;
+	}
+
+	public function getStartDate(){
+	  return $this->startDate;
+	}
+
+                        	                   			private $endDate;
+    	                        
+	public function setEndDate($endDate){
+		$this->endDate = $endDate;
+         $this->apiParas["endDate"] = $endDate;
+	}
+
+	public function getEndDate(){
+	  return $this->endDate;
+	}
+
+                        	                   			private $pageNum;
+    	                        
+	public function setPageNum($pageNum){
+		$this->pageNum = $pageNum;
+         $this->apiParas["pageNum"] = $pageNum;
+	}
+
+	public function getPageNum(){
+	  return $this->pageNum;
+	}
+
+                            }
+
+
+
+
+
+        
+ 
+
